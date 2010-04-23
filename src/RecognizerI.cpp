9bd366d539291ebd70ed648fa0a8f62ce04325cf
Chuck Fleming <fleming.chuck@gmail.com>
2010-04-23 15:56:43 -0700

    OMFG.

--- src/RecognizerI.cpp
+++ src/RecognizerI.cpp
@@ -1,6 +1,12 @@
 
 #include "RecognizerI.h"
 
+#include <fstream>
+
+using namespace std;
+
+char *recognize(char *);
+
 Faces RecognizerI::findFacesAndRecognizePeople( const pair<const Byte*, const Byte*>&, const Current& )
 {
 	cout << "received request findFacesAndRecognizePeople" << endl;
@@ -10,7 +16,16 @@ Faces RecognizerI::findFacesAndRecognizePeople( const pair<const Byte*, const By
 
 Faces RecognizerI::recognizeFace( const pair<const Byte*, const Byte*>& jpegFileOfFace, const Current& )
 {
-	return vector<Face>();
+	ofstream ofp("temp.jpg");
+	ofp.write((const char *)jpegFileOfFace.first, jpegFileOfFace.second - jpegFileOfFace.first);
+	ofp.close();
+
+	char *name = recognize("temp.jpg");
+	
+	Faces face;
+	face[0].name = name;
+	
+	return face;
 }
 
 void RecognizerI::learn( const pair<const Byte*, const Byte*>& jpegFileOfFace,
